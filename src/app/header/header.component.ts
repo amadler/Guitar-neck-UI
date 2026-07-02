ca9ac6b43740573251e327a3d881adeb5ef82147
amadler <madler.andrzej@gmail.com>
2026-07-02 20:29:36 +0200

    feat: add help modal to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  helpModalOpen = false;
 
+  toggleHelpModal(): void {
+    this.helpModalOpen = !this.helpModalOpen;
+  }
 }
