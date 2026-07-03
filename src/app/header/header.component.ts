6fe425a595ab992be14a25d8956408f0573e2106
amadler <madler.andrzej@gmail.com>
2026-07-03 20:03:35 +0200

    feat: improve help button visibility and add first-visit popup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { Component, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import { NgIf, isPlatformBrowser } from '@angular/common';
+
+const STORAGE_KEY = 'guitar-neck-ui-visited';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +10,17 @@ import { NgIf } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  private readonly platformId = inject(PLATFORM_ID);
+
   helpModalOpen = false;
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId) && !localStorage.getItem(STORAGE_KEY)) {
+      this.helpModalOpen = true;
+      localStorage.setItem(STORAGE_KEY, 'true');
+    }
+  }
 
   toggleHelpModal(): void {
     this.helpModalOpen = !this.helpModalOpen;
